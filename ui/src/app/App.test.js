ed1d19273d91f23f602d5a002b737bb5d15d7621
Alex Hall <james_a_hall@homedepot.com>
2019-06-14 13:47:52 -0400

    Fix tests...

--- ui/src/app/App.test.js
+++ ui/src/app/App.test.js
@@ -4,23 +4,29 @@ import App from './App'
 import { Route } from 'react-router-dom'
 import Landing from '../landing/Landing'
 import Login from '../login/Login'
+import WishGalaxy from '../wish-galaxy/wishGalaxy'
 
 describe('Default routing behavior', () => {
+  const findRoute = (wrapper, index) =>
+    wrapper
+      .find(Route)
+      .at(index)
+      .props()
+
   it('renders the login page by default', () => {
     const wrapper = shallow(<App />)
 
-    let loginRoute = wrapper
-      .find(Route)
-      .at(1)
-      .props()
+    const loginRoute = findRoute(wrapper, 2)
 
     expect(loginRoute.path).toEqual('/')
     expect(loginRoute.component).toEqual(Login)
 
-    let landingRoute = wrapper
-      .find(Route)
-      .at(0)
-      .props()
+    const wishGalaxyRoute = findRoute(wrapper, 1)
+
+    expect(wishGalaxyRoute.path).toEqual('/wish-galaxy')
+    expect(wishGalaxyRoute.component).toEqual(WishGalaxy)
+
+    const landingRoute = findRoute(wrapper, 0)
 
     expect(landingRoute.path).toEqual('/landing')
     expect(landingRoute.component).toEqual(Landing)
